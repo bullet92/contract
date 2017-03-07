42a7fa4b7528d752b076c041b0c72dc2161492a5
Fabrizio Pappolla <bullet.sh23@yahoo.it>
2017-03-07 20:47:29 +0100

    Deleted lang support
    Deleted lang support, need to be fixed

--- contract_recurring_invoicing_marker/models/account_analytic_account.py
+++ contract_recurring_invoicing_marker/models/account_analytic_account.py
@@ -28,16 +28,13 @@ class AccountAnalyticAccount(models.Model):
 
     @api.model
     def _insert_markers(self, line, date_start, date_end, date_format):
-	lang_obj = self.env['res.lang']
-	contract = line.analytic_account_id
-	lang = lang_obj.search(
-		[('code', '=', contract.partner_id.lang)])
+
         line = line.replace('#START#', date_start.strftime(date_format))
         line = line.replace('#END#', date_end.strftime(date_format))
         # Invoiced period  need to start to next month
         date_start = date(2017, int(date_start.strftime( '%m' )) +1 , 1 )
 	try: 
-		lang
+	lang
 	except NameError:
 			lang='it'
         line = line.replace('#START_MONTH#', format_date(date_start, "MMMM" , locale=lang).upper())
